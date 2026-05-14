579738a94afe3375433fe862c03a7384fa6b04e5
Eugene <nightingale.solist@gmail.com>
2026-05-14 20:05:55 +0000

    deleted priority from network.go in go-repository

--- src/incus/generated_prototype/network_put.rs
+++ src/incus/generated_prototype/network_put.rs
@@ -5,6 +5,4 @@ use crate::incus::ConfigMap;
 pub struct NetworkPut {
     pub config: ConfigMap,
     pub description: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub priority: Option<i64>,
 }
--- src/incus/generated_prototype/networks_post.rs
+++ src/incus/generated_prototype/networks_post.rs
@@ -5,5 +5,4 @@ use crate::incus::ConfigMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct NetworksPost {pub name: String,#[serde(rename = "type")]
-pub r#type: String,pub config: ConfigMap,pub description: String,#[serde(skip_serializing_if = "Option::is_none")]
-pub priority: Option<i64>,}
+pub r#type: String,pub config: ConfigMap,pub description: String,}
